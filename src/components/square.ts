5a56b26a6703b7a8b556b250bd0dc0bc3650bccb
jaw163 <jwhite162@protonmail.com>
2023-09-15 20:14:03 +0100

    Forgot to fix error in square.ts file

--- src/components/square.ts
+++ src/components/square.ts
@@ -6,6 +6,17 @@ import { WorldUniforms } from "./world/worldUniforms";
 export default class Square {
   public square: WithCoordsField;
   public shader;
+  uniforms = {
+    ...WorldUniforms.values,
+    u_plane_height: {
+      type: "float",
+      value: 2.0,
+    },
+    u_plane_width: {
+      type: "float",
+      value: 2.0,
+    },
+  };
 
   constructor() {
     this.shader = new NormalShader();
@@ -38,24 +49,12 @@ export default class Square {
 
     const geometry = new THREE.ShapeGeometry(shape);
     const material = new THREE.ShaderMaterial({
-      uniforms: {
-        ...WorldUniforms.values,
-        u_plane_height: {
-          type: "float",
-          value: 2.0,
-        },
-        u_plane_width: {
-          type: "float",
-          value: 2.0,
-        },
-      },
+      uniforms: this.uniforms,
       vertexShader: this.shader.vertex,
       fragmentShader: this.shader.fragment,
-      // wireframe: true,
     });
 
     const mesh = new THREE.Mesh(geometry, material);
-    // mesh.position.set(0, 0, -120);
 
     mesh.position.y += 0.2;
 
